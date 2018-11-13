b5154356432ebac092b54cb9ca29d68038b4c8c7
softyy <adkincj@gmail.com>
2018-11-12 23:50:02 -0500

    init commit

---
+++ webapp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import dash
+
+from .templates import index
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = index.render()
